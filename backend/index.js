f9625174bcdd93fc9d861b2d664f1d303d4ead2a
Zeel Patel <zeelpatel6878@gmail.com>
2025-01-03 12:05:25 +0530

    first commit

--- backend/index.js
+++ backend/index.js
@@ -22,6 +22,6 @@ app.use('/api/doctor',doctorRouter)
 app.use('/api/appointment',appointmentRouter)
 app.use('/api/patient',patientRouter)
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+app.listen(4000, () => {
+    console.log('Server is running on port 4000');
 })
